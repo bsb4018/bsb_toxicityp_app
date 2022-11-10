5939accbd33295f88b3c3074f782b0521854f873
Shibendra Bhattacharjee <storebshiv@gmail.com>
2022-11-10 22:33:51 +0530

    Data Ingestion Stage Completed

--- toxicpred/constant/training_pipeline/__init__.py
+++ toxicpred/constant/training_pipeline/__init__.py
@@ -21,7 +21,6 @@ SCHEMA_DROP_COLS = "drop_columns"
 '''
 Data Ingestion related constant start with DATA_INGESTION VAR NAME
 '''
-#DATA_INGESTION_COLLECTION_NAME: str = "car"
 DATA_INGESTION_DIR_NAME: str = "data_ingestion"
 DATA_INGESTION_FEATURE_STORE_DIR: str = "feature_store"
 DATA_INGESTION_INGESTED_DIR: str = "ingested"
